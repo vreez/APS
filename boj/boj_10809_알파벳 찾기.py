d8d9c142485ea7d63df99f6b5fa7f613b14cafa8
vreez <dearsoou@gmail.com>
2021-07-28 23:38:29 +0900

    [python] APS boj_10809_알파벳 찾기

---
+++ boj/boj_10809_알파벳 찾기.py
@@ -0,0 +1,17 @@
+import sys; sys.stdin = open("input/10809.txt", "r")
+
+alpha = {
+    'a': -1, 'b': -1, 'c': -1, 'd': -1, 'e': -1, 'f': -1,
+    'g': -1, 'h': -1, 'i': -1, 'j': -1, 'k': -1, 'l': -1, 'm': -1,
+    'n': -1, 'o': -1, 'p': -1, 'q': -1, 'r': -1, 's': -1,
+    't': -1, 'u': -1, 'v': -1, 'w': -1, 'x': -1, 'y': -1, 'z': -1
+}
+
+word = list(input())
+
+for i in range(len(word)):
+    if alpha[word[i]] == -1:
+        alpha[word[i]] = i
+
+for i in range(97, 123):
+    print(alpha[chr(i)], end=' ')
