e31033bc83f295611d41d1d68c85c837c71dc19e
vreez <dearsoou@gmail.com>
2021-08-25 22:24:28 +0900

    [python] APS_boj_2164_카드2

---
+++ boj/boj_2164_카드2.py
@@ -0,0 +1,13 @@
+import sys; sys.stdin = open("2164.txt", "r")
+from collections import deque
+
+N = int(input())
+cards = deque([])
+for i in range(1, N+1):
+    cards.append(i)
+
+while len(cards) > 1:
+    cards.popleft()
+    cards.append(cards.popleft())
+
+print(cards[0])
