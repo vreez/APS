566055dc288c5eeecc00cf34a6b021c34fc4c09b
vreez <dearsoou@gmail.com>
2021-10-10 20:16:23 +0900

    [python] APS_boj_10808_알파벳 개수

---
+++ boj/boj_10808_알파벳 개수.py
@@ -0,0 +1,19 @@
+import sys; sys.stdin = open("10808.txt", "r")
+
+alpha = {
+    'a':0, 'b':0, 'c':0, 'd':0, 'e':0, 'f':0, 'g':0, 'h':0,
+    'i':0, 'j':0, 'k':0, 'l':0, 'm':0, 'n':0, 'o':0, 'p':0,
+    'q':0, 'r':0, 's':0, 't':0, 'u':0, 'v':0, 'w':0, 'x':0,
+    'y':0, 'z':0
+    }
+word = list(input())
+for i in range(len(word)):
+    alpha[word[i]] += 1
+
+dict = sorted(alpha.items())
+
+for i in range(len(dict)):
+    if i < (len(dict) - 1):
+        print(dict[i][1], end=' ')
+    else:
+        print(dict[i][1])
