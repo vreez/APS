825529de2290dc4574cc09ee7cdd040585920710
vreez <dearsoou@gmail.com>
2021-11-09 22:36:39 +0900

    [python] boj_7600_문자가 몇갤까

---
+++ boj/boj_7600_문자가 몇갤까.py
@@ -0,0 +1,25 @@
+import sys; sys.stdin = open("7600.txt", "r")
+
+while True:
+    words = input()
+    if words == "#":
+        break
+    else:
+        alpha = {
+            'a': 0, 'b': 0, 'c': 0, 'd': 0, 'e': 0, 'f': 0, 'g': 0,
+            'h': 0, 'i': 0, 'j': 0, 'k': 0, 'l': 0, 'm': 0, 'n': 0,
+            'o': 0, 'p': 0, 'q': 0, 'r': 0, 's': 0, 't': 0, 'u': 0,
+            'v': 0, 'w': 0, 'x': 0, 'y': 0, 'z': 0
+        }
+
+        string = list(words)
+        for i in range(len(string)):
+            if string[i].lower() in alpha:
+                alpha[string[i].lower()] += 1
+
+        count = 0
+        for key, value in alpha.items():
+            if value > 0:
+                count += 1
+
+        print(count)
