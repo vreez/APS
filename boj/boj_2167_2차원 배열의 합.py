ff81c734e397d188b9df784a1c454288d7187c8e
vreez <dearsoou@gmail.com>
2021-12-19 16:08:39 +0900

    [python] boj_2167_2차원 배열의 합

---
+++ boj/boj_2167_2차원 배열의 합.py
@@ -0,0 +1,18 @@
+import sys; sys.stdin = open("2167.txt", "r")
+
+N, M = map(int, input().split())
+arr = []
+for _ in range(N):
+    arr.append(list(map(int, input().split())))
+dp = [[0] * (M + 1) for _ in range(N + 1)]
+
+for i in range(1, N + 1):
+    for j in range(1, M + 1):
+        dp[i][j] = arr[i-1][j-1] + dp[i-1][j] + dp[i][j-1] - dp[i-1][j-1]
+
+K = int(input())
+for _ in range(K):
+    i, j, x, y = map(int, input().split())
+    print(dp[x][y] - dp[x][j-1] - dp[i-1][y] + dp[i-1][j-1])
+
+
