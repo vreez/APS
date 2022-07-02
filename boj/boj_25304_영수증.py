40f049419d25ed52e3580ff9e385f980d55e61f1
vreez <dearsoou@gmail.com>
2022-07-02 09:44:49 +0900

    [python] boj_25304_영수증

---
+++ boj/boj_25304_영수증.py
@@ -0,0 +1,13 @@
+import sys; sys.stdin = open("25304.txt", "r")
+
+X = int(input())
+N = int(input())
+total = 0
+for i in range(N):
+    a, b = map(int, input().split())
+    total += (a * b)
+
+if X == total:
+    print("Yes")
+else:
+    print("No")
