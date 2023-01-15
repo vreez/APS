1daf1a8e9083831aae665e3f7b5081cf201176fe
vreez <dearsoou@gmail.com>
2023-01-15 13:20:16 +0900

    [python] boj_1110_더하기 사이클

---
+++ boj/boj_1110_더하기 사이클.py
@@ -0,0 +1,14 @@
+import sys; sys.stdin = open("1110.txt", "r")
+
+N = int(input())
+num = N
+cnt = 0
+while True:
+    a = num // 10
+    b = num % 10
+    c = (a + b) % 10
+    num = b * 10 + c
+    cnt += 1
+    if num == N:
+        break
+print(cnt)
