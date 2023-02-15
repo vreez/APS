aa28034ecf73383cfaec12c3712961e8a75a0ef1
vreez <dearsoou@gmail.com>
2023-02-15 20:40:04 +0900

    [python]boj_2445_별 찍기 - 8

---
+++ boj/boj_2445_별 찍기 - 8.py
@@ -0,0 +1,9 @@
+import sys; sys.stdin = open("2445.txt", "r")
+
+N = int(input())
+
+for i in range(1, N+1):
+    print("*" * i + (" " * (N - i) * 2) + "*" * i)
+
+for i in range(N-1, 0, -1):
+    print("*" * i + (" " * (N - i) * 2) + "*" * i)
