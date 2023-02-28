2d070ea231cd6aa19057a881796bbc7cc24d6145
vreez <dearsoou@gmail.com>
2023-02-28 22:20:59 +0900

    [python]boj_26767_Hurra

---
+++ boj/boj_26767_Hurra!.py
@@ -0,0 +1,13 @@
+import sys; sys.stdin = open("26767.txt", "r")
+
+N = int(input())
+
+for i in range(1, N+1):
+    if i % 7 == 0 and i % 11 != 0:
+        print("Hurra!")
+    elif i % 7 != 0 and i % 11 == 0:
+        print("Super!")
+    elif i % 7 == 0 and i % 11 == 0:
+        print("Wiwat!")
+    else:
+        print(i)
