0d0af64d1c812f3ac99f7b78d4c581a58591ef9e
vreez <dearsoou@gmail.com>
2023-06-24 01:06:47 +0900

    [python]boj_28235_코드마스터 2023

---
+++ boj/boj_28235_코드마스터 2023.py
@@ -0,0 +1,11 @@
+import sys; sys.stdin = open("28235.txt", "r")
+
+word = input()
+if word == "SONGDO":
+    print("HIGHSCHOOL")
+elif word == "CODE":
+    print("MASTER")
+elif word == "2023":
+    print("0611")
+else:
+    print("CONTEST")
