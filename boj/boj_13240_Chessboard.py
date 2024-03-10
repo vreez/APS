2bac30df6e00ad9fab6544130675afd80af0db3b
vreez <dearsoou@gmail.com>
2024-03-10 11:53:48 +0900

    [python]boj_13240_Chessboard

---
+++ boj/boj_13240_Chessboard.py
@@ -0,0 +1,16 @@
+import sys; sys.stdin = open("13240.txt", "r")
+n, m = map(int, input().split())
+for i in range(n):
+    for j in range(m):
+        if i % 2 == 0:
+            if j % 2 == 0:
+                print("*", end="")
+            else:
+                print(".", end="")
+        else:
+            if j % 2 == 0:
+                print(".", end="")
+            else:
+                print("*", end="")
+    print()
+
