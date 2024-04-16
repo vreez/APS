327c058afac80ccb5536dec0889e9f3b25cf1d0c
vreez <dearsoou@gmail.com>
2024-04-17 06:24:54 +0900

    [python]boj_28960_망토

---
+++ boj/boj_28960_망토.py
@@ -0,0 +1,7 @@
+import sys; sys.stdin = open("28960.txt", "r")
+
+h, l, a, b = map(int, input().split())
+if (h >= a / 2 and l >= b) or (h >= b / 2 and l >= a):
+    print("YES")
+else:
+    print("NO")
