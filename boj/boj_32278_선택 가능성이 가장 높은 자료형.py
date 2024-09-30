2e4d60ec56e0a569e8d8489a0cd5bbd758d75c35
vreez <dearsoou@gmail.com>
2024-09-30 21:05:32 +0900

    [python]boj_32278_선택 가능성이 가장 높은 자료형

---
+++ boj/boj_32278_선택 가능성이 가장 높은 자료형.py
@@ -0,0 +1,9 @@
+import sys; sys.stdin = open("32278.txt", "r")
+
+N = int(input())
+if -32768 <= N <= 32767:
+    print("short")
+elif -2147483648 <= N <= 2147483647:
+    print("int")
+else:
+    print("long long")
