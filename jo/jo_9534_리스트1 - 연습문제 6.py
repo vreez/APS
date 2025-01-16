3e63b66426453866228352ba8e65677822c36ac0
vreez <dearsoou@gmail.com>
2025-01-16 19:01:41 +0900

    [python]jo_9534_리스트1 - 연습문제 6

---
+++ jo/jo_9534_리스트1 - 연습문제 6.py
@@ -0,0 +1,7 @@
+li = ['Carrot', 'Onion', 'Cabbage', 'Potato', 'Sweet Potato', 'Bean']
+print("첫 번째 원소: {}".format(li[0]))
+print("마지막 원소: {}".format(li[-1]))
+print("리스트 거꾸로 출력하기: {}".format(li[::-1]))
+print("li[2:5]: {}".format(li[2:5]))
+print("li[5:2:-1]: {}".format(li[5:2:-1]))
+print("li[1:6:2]: {}".format(li[1:6:2]))
