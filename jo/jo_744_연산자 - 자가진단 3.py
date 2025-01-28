638d71378cc3e3fc19e9614323a5df5e630da16d
vreez <dearsoou@gmail.com>
2025-01-28 12:43:22 +0900

    [python]jo_744_연산자 - 자가진단 3

---
+++ jo/jo_744_연산자 - 자가진단 3.py
@@ -0,0 +1,14 @@
+import sys; sys.stdin = open("744.txt", "r")
+
+a = int(input())
+b = int(input())
+
+if a == b:
+    print("True")
+else:
+    print("False")
+
+if a != b:
+    print("True")
+else:
+    print("False")
