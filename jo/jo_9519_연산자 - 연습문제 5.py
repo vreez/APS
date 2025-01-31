504909c820381d56cc9a625e3d3af581757abda8
vreez <dearsoou@gmail.com>
2025-01-31 12:09:26 +0900

    [python]jo_9519_연산자 - 연습문제 5

---
+++ jo/jo_9519_연산자 - 연습문제 5.py
@@ -0,0 +1,23 @@
+a = False
+b = True
+c = True
+
+if a == True and b == True:
+    print("True", end=" ")
+else:
+    print("False", end=" ")
+
+if a == True or b == True:
+    print("True", end=" ")
+else:
+    print("False", end=" ")
+
+if c == True and b == True:
+    print("True", end=" ")
+else:
+    print("False", end=" ")
+
+if a == False:
+    print("True", end=" ")
+else:
+    print("False", end=" ")
