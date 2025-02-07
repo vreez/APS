ceb85cd4556696127b3886f062cf610fcf27cf46
vreez <dearsoou@gmail.com>
2025-02-07 21:25:26 +0900

    [python]jo_9512_변수와 입력

---
+++ jo/jo_9512_변수와 입력 - 연습문제 7.py
@@ -0,0 +1,10 @@
+import sys; sys.stdin = open("9512.txt", "r")
+
+h = int(input())
+w = float(input())
+n = input()
+
+print("키를 입력하시오. 몸무게를 입력하시오. 이름을 입력하시오.", end=" ")
+print("키 = {}".format(h))
+print("몸무게 = {}".format(w))
+print("이름 = {}".format(n))
