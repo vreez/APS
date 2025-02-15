59144333e1711063d235cf0912dccf96121bdc3b
vreez <dearsoou@gmail.com>
2025-02-16 00:20:14 +0900

    [python]jo_9511_변수와 입력 - 연습문제 6

---
+++ jo/jo_9511_변수와 입력 - 연습문제 6.py
@@ -0,0 +1,8 @@
+import sys; sys.stdin = open("9511.txt", "r")
+
+a = int(input())
+b = int(input())
+print("수를 입력하시오.", end=" ")
+print("수를 입력하시오.", end=" ")
+print("{} + {} = {}".format(a, b, a + b))
+print("{} * {} = {}".format(a, b, a * b))
