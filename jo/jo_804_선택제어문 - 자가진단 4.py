fd433bac6b6abbdf19f12a6f10815ccc78fd4ba6
YOONVIN KIM <68372536+vreez@users.noreply.github.com>
2025-04-08 19:58:12 +0900

    [python]jo_804_선택제어문 - 자가진단 4

---
+++ jo/jo_804_선택제어문 - 자가진단 4.py
@@ -0,0 +1,12 @@
+N = float(input())
+
+if N > 88.45:
+    print("Heavyweight")
+elif N > 72.57:
+    print("Cruiserweight")
+elif N > 61.23:
+    print("Middleweight")
+elif N > 50.80:
+    print("Lightweight")
+else:
+    print("Flyweight")
