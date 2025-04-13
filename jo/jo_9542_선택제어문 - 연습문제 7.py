615484d7be5b669b282f2a5ddc6b4b2693890b76
YOONVIN KIM <68372536+vreez@users.noreply.github.com>
2025-04-13 11:05:51 +0900

    [python]jo_9542_선택제어문 - 연습문제 7

---
+++ jo/jo_9542_선택제어문 - 연습문제 7.py
@@ -0,0 +1,11 @@
+print("1. 삽입")
+print("2. 수정")
+print("3. 삭제")
+print("메뉴를 선택하세요.", end=" ")
+N = int(input())
+if N == 1:
+    print("삽입을 선택하셨습니다.")
+elif N == 2:
+    print("수정을 선택하셨습니다.")
+else:
+    print("삭제를 선택하셨습니다.")
