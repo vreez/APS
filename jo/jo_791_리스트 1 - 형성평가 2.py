6d69a6033b15cd5e8ad6f02b2081ff14a48ff0c1
YOONVIN KIM <68372536+vreez@users.noreply.github.com>
2025-06-19 20:52:30 +0900

    [python]jo_791_리스트 1 - 형성평가 2

---
+++ jo/jo_791_리스트 1 - 형성평가 2.py
@@ -0,0 +1,8 @@
+arr = []
+num1, num2 = map(int, input().split())
+num3, num4 = map(int, input().split())
+for i in range(num2):
+    arr.append(num1)
+for i in range(num4):
+    arr.append(num3)
+print(arr)
