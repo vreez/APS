a05dbc6fa0a4a0d85881c2409cd7f1c75d4a2dae
YOONVIN KIM <68372536+vreez@users.noreply.github.com>
2025-06-20 01:51:53 +0900

    [python]jo_792_리스트 1 - 형성평가 3

---
+++ jo/jo_792_리스트 1 - 형성평가 3.py
@@ -0,0 +1,10 @@
+C1, A1 = input().split()
+C2, A2 = input().split()
+C3, A3 = input().split()
+C4, A4 = input().split()
+color = [C1, C2, C3, C4]
+animal = [A1, A2, A3, A4]
+print("Input? Input? Input? Input?", end=" ")
+print("Color: {}".format(color))
+print("Animal: {}".format(animal))
+
