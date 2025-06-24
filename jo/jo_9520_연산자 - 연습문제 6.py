2fb018e835ca5d3c1c2117f0a68a64b15b19b4ff
YOONVIN KIM <68372536+vreez@users.noreply.github.com>
2025-06-25 01:10:38 +0900

    [python]jo_9520_연산자 - 연습문제 6

---
+++ jo/jo_9520_연산자 - 연습문제 6.py
@@ -0,0 +1,20 @@
+a = 0
+b = 1
+c = 2
+
+if a != b:
+    print(True, end=" ")
+else:
+    print(False, end=" ")
+if b != c:
+    print(True, end=" ")
+else:
+    print(False, end=" ")
+if a == c:
+    print(True, end=" ")
+else:
+    print(False, end=" ")
+if a != b and b!= c:
+    print(True, end=" ")
+else:
+    print(False, end=" ")
