3fbb300c868167be8cf9d6b49fde607f0e8332f7
YOONVIN KIM <68372536+vreez@users.noreply.github.com>
2025-06-26 21:30:01 +0900

    [python]jo_747_연산자 - 자가진단 6

---
+++ jo/jo_747_연산자 - 자가진단 6.py
@@ -0,0 +1,13 @@
+a = int(input())
+b = int(input())
+c = int(input())
+
+if a > b and a > c:
+    print(True, end=" ")
+else:
+    print(False, end=" ")
+
+if a == b and b == c:
+    print(True, end=" ")
+else:
+    print(False, end=" ")
