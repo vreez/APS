4a2c61408346535b794cad15397844bcdd8754e0
YOONVIN KIM <68372536+vreez@users.noreply.github.com>
2025-07-03 18:58:16 +0900

    [python]jo_879_문자열 1- 자가진단 6

---
+++ jo/jo_879_문자열 1 - 자가진단 6.py
@@ -0,0 +1,6 @@
+print("""
+      item     count     price      rate
+       pen        20       100      50.5
+      note         5        95      35.3
+    eraser       110        97      14.2
+""")
