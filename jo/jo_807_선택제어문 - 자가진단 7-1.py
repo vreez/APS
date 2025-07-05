4e63d2372ec90d1b43265d43166788b8c9537f43
YOONVIN KIM <68372536+vreez@users.noreply.github.com>
2025-07-05 12:26:00 +0900

    [python]jo_807_선택제어문 - 자가진단 7-1

---
+++ jo/jo_807_선택제어문 - 자가진단 7-1.py
@@ -0,0 +1,13 @@
+alpha = input()
+if alpha == "A":
+    print("Excellent")
+elif alpha == "B":
+    print("Good")
+elif alpha == "C":
+    print("Usually")
+elif alpha == "D":
+    print("Effort")
+elif alpha == "F":
+    print("Failure")
+else:
+    print("Mistake")
