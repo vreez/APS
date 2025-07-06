87d7e062215ab937fe64165e47dc253fc4c07626
YOONVIN KIM <68372536+vreez@users.noreply.github.com>
2025-07-06 14:07:53 +0900

    [python]jo_808_선택제어문 - 자가진단 7-2

---
+++ jo/jo_808_선택제어문 - 자가진단 7-2.py
@@ -0,0 +1,10 @@
+score = float(input())
+
+if score >= 4.0:
+    print("scholarship")
+elif score >= 3.0:
+    print("next semester")
+elif score >= 2.0:
+    print("seasonal semester")
+else:
+    print("retake")
