f8a4812d13511e24af06360750d9d08ae2ae9ec9
YOONVIN KIM <68372536+vreez@users.noreply.github.com>
2025-08-08 01:00:37 +0900

    [python]jo_969_함수 1 - 형성평가 1

---
+++ jo/jo_969_함수 1 - 형성평가 1.py
@@ -0,0 +1,11 @@
+def func(n):
+    if n == 0:
+        print("first")
+    elif n == 1:
+        print("second")
+    else:
+        print("third")
+    print("@@@@@@@@@@")
+
+for i in range(3):
+    func(i)
