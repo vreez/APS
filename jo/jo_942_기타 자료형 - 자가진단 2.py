1cf946acb38d8bf5be1c00789e13613665d967e3
YOONVIN KIM <68372536+vreez@users.noreply.github.com>
2025-08-28 22:41:45 +0900

    [python]jo_942_기타 자료형 - 자가진단 2

---
+++ jo/jo_942_기타 자료형 - 자가진단 2.py
@@ -0,0 +1,9 @@
+tp1 = ('Dolphin','Oh My Girl')
+tp2 = ('Pallette','IU')
+tp3 = ('Yes or Yes', 'Twice')
+
+print("[Song by Artist]")
+print("==========")
+print("{} by {}".format(tp1[0], tp1[1]))
+print("{} by {}".format(tp2[0], tp2[1]))
+print("{} by {}".format(tp3[0], tp3[1]))
