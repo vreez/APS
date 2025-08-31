455e0cbd053d21913b572c7cdb88a4c4c50bf7fd
YOONVIN KIM <68372536+vreez@users.noreply.github.com>
2025-08-31 18:06:28 +0900

    [python]jo_9605_기타 자료형 - 연습문제 4

---
+++ jo/jo_9605_기타 자료형 - 연습문제 4.py
@@ -0,0 +1,7 @@
+tp1 = {1, 2, 3, 4, 5}
+tp2 = {3, 4, 5, 6, 7}
+
+print("교집합: {}".format(tp1 & tp2))
+print("합집합: {}".format(tp1 | tp2))
+print("A-B: {}".format(tp1 - tp2))
+print("B-A: {}".format(tp2 - tp1))
