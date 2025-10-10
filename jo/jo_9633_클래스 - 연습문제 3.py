3d9df8a4a036a1dc69c4ba8072840e5f8b3070fa
YOONVIN KIM <68372536+vreez@users.noreply.github.com>
2025-10-11 08:50:34 +0900

    [python] jo_9633_클래스 - 연습문제 3

---
+++ jo/jo_9633_클래스 - 연습문제 3.py
@@ -0,0 +1,6 @@
+n1, k1, e1 = input().split()
+n2, k2, e2 = input().split()
+
+print("{} {} {}".format(n1, k1, e1))
+print("{} {} {}".format(n2, k2, e2))
+print("{} {} {}".format("합계", int(k1) + int(k2), int(e1) + int(e2)))
