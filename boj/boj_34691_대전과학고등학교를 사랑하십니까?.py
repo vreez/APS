b1849d346aea8d23f8e30ca65764674f3549bdf5
YOONVIN KIM <68372536+vreez@users.noreply.github.com>
2025-12-11 01:27:31 +0900

    [python]boj_34691_대전과학고등학교를 사랑하십니까?

---
+++ boj/boj_34691_대전과학고등학교를 사랑하십니까?.py
@@ -0,0 +1,11 @@
+while True:
+    word = input()
+    if word == "end":
+        break
+    else:
+        if word == "animal":
+            print("Panthera tigris")
+        elif word == "tree":
+            print("Pinus densiflora")
+        else:
+            print("Forsythia koreana")
