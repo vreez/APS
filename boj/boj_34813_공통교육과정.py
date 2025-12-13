070ef62edb4dde46caf6c3e9e2c697b796ed9a51
YOONVIN KIM <68372536+vreez@users.noreply.github.com>
2025-12-14 03:39:44 +0900

    [python]boj_34813_공통교육과정

---
+++ boj/boj_34813_공통교육과정.py
@@ -0,0 +1,10 @@
+word = list(input())
+
+if word[0] =="F":
+    print("Foundation")
+elif word[0] =="C":
+    print("Claves")
+elif word[0] == "V":
+    print("Veritas")
+else:
+    print("Exploration")
