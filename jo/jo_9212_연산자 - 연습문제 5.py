d97592a9777fd913daadf5ab9f9fb9d536084b28
YOONVIN KIM <68372536+vreez@users.noreply.github.com>
2026-03-23 18:52:38 +0900

    [python]jo_9212_연산자 - 연습문제 5

---
+++ jo/jo_9212_연산자 - 연습문제 5.py
@@ -0,0 +1,14 @@
+A, B, C = True, False, True
+
+if A and B:
+    print(True, end=" ")
+else:
+    print(False, end=" ")
+if B or C:
+    print(True, end=" ")
+else:
+    print(False, end=" ")
+if B == False:
+    print(True, end=" ")
+else:
+    print(False, end=" ")
