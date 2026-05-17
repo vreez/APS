c5ce43ee8cafb07317756a1d5fd56d0304850142
YOONVIN KIM <68372536+vreez@users.noreply.github.com>
2026-05-17 19:26:06 +0900

    [python]jo_9278_반복제어문1 - 자가진단 6-3

---
+++ jo/jo_9278_반복제어문1 - 자가진단 6-3.py
@@ -0,0 +1,11 @@
+while True:
+    num = int(input())
+    if num != 1 and num != 2 and num != 3:
+        break
+    else:
+        if num == 1:
+            print("one")
+        elif num == 2:
+            print("two")
+        elif num == 3:
+            print("three")
